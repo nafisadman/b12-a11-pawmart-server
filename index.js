14901e2ea00e1713933e8ef26b78116153a15400
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-12-06 14:21:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,107 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://missionscic:VvQ8mMWfgrUtZZ9X@cluster0.12vlvst.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("petService");
+    const petServices = database.collection("services");
+
+    // post or save service to DB
+    app.post("/services", async (req, res) => {
+      const data = req.body;
+      const date = new Date();
+      data.createdAt = date;
+      console.log(data);
+      const result = await petServices.insertOne(data);
+      res.send(result);
+    });
+
+    // get services from DB
+    app.get("/services", async (req, res) => {
+      const {category} = req.query;
+      const query = {};
+      if(category){
+        query.category = category;
+      }
+      const result = await petServices.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const { id } = req.params;
+      console.log(id);
+
+      const query = { _id: new ObjectId(id) };
+      const result = await petServices.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/auth/my-services", async (req, res) => {
+      const { email } = req.query;
+
+      const query = { email: email };
+      const result = await petServices.find(query).toArray();
+      res.send(result);
+    });
+
+    app.put("/update/:id", async (req, res) => {
+      const data = req.body;
+      const id = req.params;
+      const query = { _id: new ObjectId(id) };
+      
+      const updateServices = {
+        $set : data
+      }
+
+      const result = await petServices.updateOne(query, updateServices);
+      res.send(result);
+    });
+
+    app.delete(`/delete/:id`, async(req, res)=>{
+      const id = req.params;
+      const query = {_id:new ObjectId(id)};
+      const result = await petServices.deleteOne(query);
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Developers");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
+});
