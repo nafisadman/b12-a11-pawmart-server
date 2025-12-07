78e4f6e1e5278f91f141f0b0e92956dd80d2a57a
Nafi Sadman Khan <sadmannafi789@gmail.com>
2025-12-07 16:58:51 +0600

    Hidden env files

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri =
-  "mongodb+srv://missionscic:VvQ8mMWfgrUtZZ9X@cluster0.12vlvst.mongodb.net/?appName=Cluster0";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.12vlvst.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
